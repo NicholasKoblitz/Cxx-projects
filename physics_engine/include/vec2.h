814b68f16d7038cd807c70d6d46cb195952665c8
NicholasKoblitz <68592588+NicholasKoblitz@users.noreply.github.com>
2024-04-24 12:09:50 -0400

    Added math function declerations

--- physics_engine/include/vec2.h
+++ physics_engine/include/vec2.h
@@ -11,5 +11,9 @@ typedef struct {
 // Vector 2 math Declerations
 Vec2 add(Vec2 a, Vec2 b);
 Vec2 sub(Vec2 a, Vec2 b);
-void scale_up(Vec2 *a, float b);     // Vector multiplcation
-void scale_down(Vec2 *a, float b);  // Vector division
+void scale_up(Vec2 &a, float b);     // Vector multiplcation
+void scale_down(Vec2 &a, float b);   // Vector division
+float length(Vec2 a);               // Returns the magnatude of a vector
+void set_length(Vec2 &a);           // Sets the magnatude of a vector
+void normalize(Vec2 &a);            // Turns existing vector to a unit vector
+float dist(Vec2 a, Vec2 b);         // Returns the distance betweb two vextors
